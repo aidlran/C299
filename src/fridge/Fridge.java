8271fbaf16351f3451be07883123b7263fd2f118
Aidan Loughran <aidlo.dev@pm.me>
2022-08-23 14:42:41 +0100

    Added documentation

--- src/fridge/Fridge.java
+++ src/fridge/Fridge.java
@@ -4,17 +4,32 @@ import java.util.HashMap;
 
 public class Fridge {
 
+    // Capacity of fridge, set during construction
     private final int capacity;
 
+    // Hash map that tracks quantity of each item
     private final HashMap<String, Integer> ITEM_MAP = new HashMap<>();
 
+    // Total count of items in fridge
     private int itemCount = 0;
 
+    /**
+     * Fridge Constructor.
+     * @param capacity How many items the fridge can hold.
+     */
     public Fridge(int capacity) {
         this.capacity = capacity;
     }
 
+    /**
+     * Add an item to the fridge.
+     * Checks capacity and updates values if checks pass.
+     * @param item Name of item to add.
+     */
     public void addItem(String item) {
+
+        item = item.toLowerCase();
+
         if (itemCount >= capacity) {
             System.out.println("Fridge is full! Cannot add " + item + ".");
             return;
@@ -30,7 +45,14 @@ public class Fridge {
         this.itemCount++;
     }
 
+    /**
+     * Remove an item from the fridge.
+     * Checks item count and updates values if checks pass.
+     * @param item Name of the item to remove.
+     */
     public void removeItem(String item) {
+
+        item = item.toLowerCase();
 
         if (itemCount <= 0) {
             System.out.println("Fridge is empty!");
@@ -50,6 +72,7 @@ public class Fridge {
         return itemCount + " item(s): " + ITEM_MAP;
     }
 
+    // Program entrypoint
     public static void main(String[] args) {
         Fridge fridge = new Fridge(50);
         System.out.println(fridge);
