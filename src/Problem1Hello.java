64685a55d32125a2976c5abb57e2554f767944bd
Aidan Loughran <aidlo.dev@pm.me>
2022-09-05 07:37:43 +0100

    Implemented problem 1

---
+++ src/Problem1Hello.java
@@ -0,0 +1,15 @@
+// Write a program that asks the user for their name and greets them with their name.
+// Modify the program such that only the users Biraj and DAS are greeted with their names.
+
+import java.util.Scanner;
+
+public class Problem1Hello {
+
+	private static Scanner SCANNER = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.print("What is your name? ");
+		final String NAME = SCANNER.nextLine();
+		System.out.println("Hello, " + NAME + ".");
+	}
+}
