acaeaadb5fb3d553ea59aa9f961e47e4cdf2c59d
Aidan Loughran <aidlo.dev@pm.me>
2022-09-18 21:36:17 +0100

    Class Roster Student DTO

---
+++ src/classroster/dto/Student.java
@@ -0,0 +1,41 @@
+package classroster.dto;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String id;
+    private String cohort;
+
+    public Student(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getCohort() {
+        return cohort;
+    }
+
+    public void setCohort(String cohort) {
+        this.cohort = cohort;
+    }
+}
