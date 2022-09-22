84a8f8647bb3950cda4c1b060d4a4f509de64189
Aidan Loughran <aidlo.dev@pm.me>
2022-09-22 22:17:30 +0100

    Vending Machine: main menu

---
+++ vending-machine/src/main/java/c299/vendingmachine/ui/View.java
@@ -0,0 +1,6 @@
+package c299.vendingmachine.ui;
+
+public interface View {
+    public void render();
+    public String getTitle();
+}
---
+++ vending-machine/src/main/java/c299/vendingmachine/ui/ViewImpl.java
@@ -0,0 +1,82 @@
+package c299.vendingmachine.ui;
+
+import c299.io.UserIO;
+import c299.vendingmachine.dto.Item;
+
+public abstract class ViewImpl implements View, UserIO {
+
+	protected UserIO io;
+
+	public ViewImpl(UserIO io) {
+		this.io = io;
+	}
+
+	public void print(String text) {
+		io.print(text);
+	}
+
+	public void displaySuccessMessage() {
+		io.displaySuccessMessage();
+	}
+
+	public void displayErrorMessage(String error) {
+		io.displayErrorMessage(error);
+	}
+
+	public String readString(String prompt) {
+		return io.readString(prompt);
+	}
+
+	public int readInt(String prompt) {
+		return io.readInt(prompt);
+	}
+
+	public int readInt(String prompt, int min, int max) {
+		return io.readInt(prompt, min, max);
+	}
+
+	public long readLong(String prompt) {
+		return io.readLong(prompt);
+	}
+
+	public long readLong(String prompt, long min, long max) {
+		return io.readLong(prompt, min, max);
+	}
+
+	public double readDouble(String prompt) {
+		return io.readDouble(prompt);
+	}
+
+	public double readDouble(String prompt, double min, double max) {
+		return io.readDouble(prompt, min, max);
+	}
+
+	public void render() {
+
+		String underline = "";
+
+		for (int i = 0; i < getTitle().length(); i++)
+			underline += '=';
+
+		io.print("\n" + underline);
+		io.print(getTitle());
+		io.print(underline);
+	}
+
+	public void printItemList(Item[] itemList) {
+		print("Products");
+		print("--------\n");
+        for (Item item : itemList) {
+			int dollars = item.getPrice() / 100;
+			int pennies = item.getPrice() % 100;
+			String priceString = dollars > 0
+
+				// Printed if >= $1
+				? "$" + dollars + "." + (pennies < 10 ? pennies + "0" : pennies)
+
+				// Printed if < $1
+				: pennies + "c";
+			print(item.getName() + " - " + priceString + " - " + item.getQuantity() + " remaining.");
+		}
+	}
+}
