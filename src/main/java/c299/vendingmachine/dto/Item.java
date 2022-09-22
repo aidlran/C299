7584b824d243f668ab3b605eef1b4b8affcde817
Aidan Loughran <aidlo.dev@pm.me>
2022-09-22 20:00:10 +0100

    DTOs

---
+++ src/main/java/c299/vendingmachine/dto/Item.java
@@ -0,0 +1,13 @@
+package c299.vendingmachine.dto;
+
+import lombok.Data;
+
+public @Data class Item {
+    /**
+     * Using integer (number of pennies)
+     * rather than risk float inaccuracies.
+     */
+    private int price;
+    private String name;
+    private int quantity;
+}
