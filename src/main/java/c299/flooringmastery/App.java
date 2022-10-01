03e4bb8d92857e53aee59c82e683c8d514421bb9
Aidan Loughran <aidlo.dev@pm.me>
2022-10-01 16:18:24 +0100

    Use Controller interface as requiredType

--- src/main/java/c299/flooringmastery/App.java
+++ src/main/java/c299/flooringmastery/App.java
@@ -3,13 +3,13 @@ package c299.flooringmastery;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import c299.flooringmastery.controller.ControllerCore;
+import c299.flooringmastery.controller.Controller;
 
 public class App {
 
 	static ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 	public static void main(String[] args) {
-		appContext.getBean("coreController", ControllerCore.class).run();
+		appContext.getBean("coreController", Controller.class).run();
 	}
 }
