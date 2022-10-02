9b99e834d81db8e660e33f80c4378950dbc96913
Aidan Loughran <aidlo.dev@pm.me>
2022-10-02 18:42:19 +0100

    Initialise

---
+++ src/main/java/c299/guessthenumber/GuessTheNumber.java
@@ -0,0 +1,11 @@
+package c299.guessthenumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessTheNumber {
+	public static void main(String[] args) {
+		SpringApplication.run(GuessTheNumber.class, args);
+	}
+}
